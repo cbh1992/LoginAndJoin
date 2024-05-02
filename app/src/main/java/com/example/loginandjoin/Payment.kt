1e06a3c608c57816994ad700abdcd8187f5878f7
92cbh <whgg9786@gmail.com>
2024-05-02 20:19:28 +0900

    Fix : Unable to start activity 오류 해결 및 결제방식 기초공사

--- app/src/main/java/com/example/loginandjoin/Payment.kt
+++ app/src/main/java/com/example/loginandjoin/Payment.kt
@@ -1,20 +1,36 @@
 package com.example.loginandjoin
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.tosspayments.paymentsdk.view.PaymentMethod
 
 class Payment : AppCompatActivity() {
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_payment)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+        val card = findViewById<Button>(R.id.PayCard)
+        card.setOnClickListener {
+            val cardpay = Intent(this,PaymentCard::class.java)
+            startActivity(cardpay)
         }
+    }
+    companion object {
+        private const val TEST_KEY = "test_ck_DLJOpm5Qrldyn50kjaoPrPNdxbWn"
+        private const val TEST_CUSTOMER_KEY = "test_sk_KNbdOvk5rk1zKEnNLOl43n07xlzm"
     }
 }
